cedfa9216b977c6cbb4277f9aec4e5ea2e4be35a
Petr Fidrmuc <petr.fidrmuc@tul.cz>
2025-10-23 14:04:52 +0200

    #5 obdelni

---
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -0,0 +1,48 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************************************");
+    Console.WriteLine("****************serazeni dvou cisel*****************");
+    Console.WriteLine("****************************************************");
+    Console.WriteLine("****************************************************");
+    Console.WriteLine("*******************Petr Fidrmuc*********************");
+    Console.WriteLine("********************2025.10.23**********************");
+    Console.WriteLine("****************************************************");
+    Console.WriteLine("****************************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadej šířku obdelniku: ");
+   
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.WriteLine("nezadal jsi cele cislo. zadej ho znovu: ");
+    }
+
+    Console.Write("Zadej vysku obdelniku (musi byt cele): ");
+   
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.WriteLine("nezadal jsi cele cislo. zadej ho znovu: ");
+    }
+
+
+
+
+    for (int i = 1; i <= height ; i++)
+    {
+        for (int j = 1; j <= width; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("pro opakovani stiskni a");
+    again = Console.ReadLine();
+
+}
